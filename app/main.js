11fe0d13c51c0367ed405a63d743bb484650fc51
Dineshkumar <dineshkumarch21989@gmail.com>
2018-03-27 11:25:46 +0530

    Added vue app
    app directory

---
+++ app/main.js
@@ -0,0 +1,8 @@
+// const Vue = require('nativescript-vue');
+// const App = require('./components/App');
+import Vue from 'nativescript-vue';
+import App from './components/App';
+new Vue({
+  render: h => h(App),
+}).$start();
+
